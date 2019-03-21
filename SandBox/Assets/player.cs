0abcf4dbb565e405708bb9766d6c98067fd15451
oukhali99 <oukhali@hotmail.com>
2019-03-21 19:32:20 -0400

    SandBox

--- SandBox/Assets/player.cs
+++ SandBox/Assets/player.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
-        
-        
+public class Player : MonoBehaviour {
+
+    public float movementMultiplier;
+
+    private Vector3 movement;
+    private Rigidbody rb;
+    private Camera cam;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        movement = new Vector3(0, 0, 0);
+        rb = gameObject.GetComponent<Rigidbody>();
+        cam = gameObject.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        movement.Set(0, 0, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            movement.y += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            movement.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement.x += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement.z -= 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement.z += 1;
+        }
         
+        rb.AddForce(movement * movementMultiplier);
+
+        
+        //Input.mousePosition
 	}
 }
