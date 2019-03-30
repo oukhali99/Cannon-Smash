eb5c55be503f186add3a820486786313308e59de
oukhali99 <42676331+oukhali99@users.noreply.github.com>
2019-03-29 20:22:10 -0400

    Worked on animating the player

---
+++ PvPGame/Assets/_Project/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
