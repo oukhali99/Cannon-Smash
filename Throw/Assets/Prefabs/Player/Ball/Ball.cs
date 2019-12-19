b4c1db9501dde40b5c996a0f18b4a0ccfc1796c1
oukhali99 <oukhali@hotmail.com>
2019-12-19 07:01:10 -0500

    commit

---
+++ Throw/Assets/Prefabs/Player/Ball/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Global.Ammo++;
+	}
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Throw/Assets/Prefabs/Player/RandomSky.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSky : MonoBehaviour
+{
+    public Material[] Skyboxes;
+
+    void Start()
+    {
+        Material chosenSkybox = Skyboxes[Mathf.FloorToInt(Random.Range(0, Skyboxes.Length))];
+        RenderSettings.skybox = chosenSkybox;
+    }
+}
