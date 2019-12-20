ee429187ee37ce4a4d31a721c4617750fc575048
oukhali99 <oukhali@hotmail.com>
2019-12-20 16:32:47 -0500

    commit

---
+++ Throw/Assets/Project/Player/InGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGame : MonoBehaviour
+{
+    public static InGame Instance;
+
+    public bool Verbose;
+    public int Score;
+    public int MaxScore;
+    public int Ammo;
+    public Pooler BallPooler;
+    public TMPro.TextMeshProUGUI ScoreText;
+    public TMPro.TextMeshProUGUI AmmoText;
+    public Camera MyCamera;
+    public UnityEngine.UI.CanvasScaler MyCanvasScaler;
+    public float HeightFactor;
+    public string GameOptionsFileName;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Instance = this;
+        Application.targetFrameRate = 30;
+	}
+
+    void Start()
+    {
+        Instance.Ammo = BallPooler.Pool.Length;
+        UpdateScoreboard();
+
+        // Canvas Size
+        Static.ScaleUI(MyCamera, MyCanvasScaler, HeightFactor, GameOptionsFileName);
+    }
+
+    public void UpdateScoreboard()
+    {
+        Instance.ScoreText.text = (Static.Round(100 * (float)Instance.Score / Instance.MaxScore, 1)).ToString() + "%";
+        Instance.AmmoText.text = Instance.Ammo.ToString();
+    }
+
+    public void ClickedReset()
+    {
+        Static.ReloadScene();
+    }
+    public void ClickedMenu()
+    {
+        Static.LoadScene(0);
+    }
+
+    public void Log(string str)
+    {
+        if (Verbose)
+        {
+            Debug.Log(str);
+        }
+    }
+}
---
+++ Throw/Assets/Project/Static.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Static
+{
+    public static float Round(float n, int dec)
+    {
+        for (int i = 0; i < dec; i++)
+        {
+            n *= 10;
+        }
+
+        n = Mathf.Floor(n);
+
+        for (int i = 0; i < dec; i++)
+        {
+            n /= 10;
+        }
+
+        return n;
+    }
+
+    public static void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public static void ReloadScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public static void Log(bool verbose, string str)
+    {
+        if (verbose)
+        {
+            Debug.Log(str);
+        }
+    }
+
+    public static void ScaleUI(Camera cam, CanvasScaler cs, float heightFactor, string gameOptionsPath)
+    {
+        float resHeight = cam.pixelHeight;
+        cs.scaleFactor = resHeight * Options.LoadGameOptions(gameOptionsPath).UIScale / heightFactor;
+    }
+}
