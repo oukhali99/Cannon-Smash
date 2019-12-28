9e94abd5579e75cfd771ad4b15d0c5d0d6764006
oukhali99 <42676331+oukhali99@users.noreply.github.com>
2019-12-28 00:33:08 -0500

    commit

---
+++ Throw/Assets/_Project/Scenes/Common/Scripts/InGamePanels/Ammo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ammo : MonoBehaviour
+{
+    public static Ammo Instance { get; private set; }
+
+    [SerializeField] public int MaxAmmo;
+    [SerializeField] private TextMeshProUGUI Text;
+
+    public int ammo { get; set; }
+
+    void Awake()
+    {
+        ammo = 0;
+        Instance = this;
+    }
+
+    void Start()
+    {
+        RefreshText();
+    }
+    
+    public void PlayerFires()
+    {
+        ammo--;
+        RefreshText();
+    }
+    
+    public int GetAmmo()
+    {
+        return ammo;
+    }
+    
+    public void RefreshText()
+    {
+        Text.text = ammo.ToString();
+    }
+}
---
+++ Throw/Assets/_Project/Scenes/Common/Scripts/InGamePanels/AmmoWheel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmmoWheel : MonoBehaviour
+{
+    public static AmmoWheel Instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI SelectedAmmoText;
+    [SerializeField] private TextMeshProUGUI SelectedAmmoCountText;
+    [SerializeField] private string NormalAmmoName;
+    [SerializeField] private string ExplosiveAmmoName;
+    [SerializeField] private string AntigravityAmmoName;
+
+    private int selectedIndex;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        selectedIndex = 0;
+        SelectedAmmoText.text = NormalAmmoName;
+        BallPooler.Instance.SelectedAmmo = BallPooler.Instance.NormalBallList;
+        Refresh();
+    }
+
+    public void Clicked()
+    {
+        if (selectedIndex == 0)
+        {
+            SelectedAmmoText.text = ExplosiveAmmoName;
+            BallPooler.Instance.SelectedAmmo = BallPooler.Instance.ExplosiveBallList;
+            Refresh();
+            selectedIndex = 1;
+        }
+        else if (selectedIndex == 1)
+        {
+            SelectedAmmoText.text = AntigravityAmmoName;
+            BallPooler.Instance.SelectedAmmo = BallPooler.Instance.AntigravityBallList;
+            Refresh();
+            selectedIndex = 2;
+        }
+        else
+        {
+            SelectedAmmoText.text = NormalAmmoName;
+            BallPooler.Instance.SelectedAmmo = BallPooler.Instance.NormalBallList;
+            Refresh();
+            selectedIndex = 0;
+        }
+    }
+
+    public void Refresh()
+    {
+        SelectedAmmoCountText.text = BallPooler.Instance.SelectedAmmo.Count.ToString();
+    }
+}
