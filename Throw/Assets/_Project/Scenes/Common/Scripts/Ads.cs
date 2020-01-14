f5da39cc0164a3beb7a4161be2a961cce40c4355
oukhali99 <oukhali@hotmail.com>
2020-01-14 15:57:28 -0500

    commit

--- Throw/Assets/_Project/Scenes/Common/Scripts/Ads.cs
+++ Throw/Assets/_Project/Scenes/Common/Scripts/Ads.cs
@@ -5,11 +5,36 @@ using UnityEngine.Advertisements;
 
 public class Ads : MonoBehaviour
 {
-    string gameId = "3415015";
-    bool testMode = true;
+    private string gameId = "3415015";
+    private bool testMode = true;
+    private bool ready;
+
+    void Awake()
+    {
+        ready = false;
+    }
 
     void Start()
     {
         Advertisement.Initialize(gameId, testMode);
+    }
+
+    void Update()
+    {
+        if (!ready)
+        {
+            if (Advertisement.IsReady())
+            {
+                ready = true;
+            }
+        }
+    }
+
+    public void ShowAd()
+    {
+        if (ready)
+        {
+            Advertisement.Show();
+        }
     }
 }
