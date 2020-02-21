0d3ebeb523a8d47659f2cacf426476080e483c52
oukhali99 <oukhali@hotmail.com>
2020-02-21 18:37:29 -0500

    commit

--- Throw/Assets/_Project/Side Buildings/Common/Scripts/SideBuilding.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SideBuilding : MonoBehaviour
-{
-    public float ZLength;
-    public float Prevalence;
-}
